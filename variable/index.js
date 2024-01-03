c96166595a1b9a899a60924023a6a244e5334b11
bhankumari <bhankumarilamsal137@gmail.com>
2024-01-03 14:37:17 +0545

    Switch Case Practice

--- variable/index.js
+++ variable/index.js
@@ -1,15 +1,45 @@
-const name = prompt("Enter your name")
+// const name = (prompt("Enter your name"))
 
+// if(name==="Balkrishna Lamsal"){
 
-if(name==="Balkrishna Lamsal"){
+// console.log("Welcome admin")
+// alert("Welcome admin")
 
-console.log("Welcome admin")
-alert("Welcome admin")
+// }else{
 
-}else{
+// console.log("Welcome Guest")
+// alert("Welcome Guest")
 
-console.log("Welcome Guest")
-alert("Welcome Guest")
+// }
 
+const number = Number(prompt("Enter number between 1 to 7"));
+switch (number) {
+  case 1:
+    console.log("Sunday");
 
+    break;
+  case 2:
+    console.log("Sunday");
+
+    break;
+  case 3:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 3:
+    console.log("Thrusday");
+    break;
+  case 3:
+    console.log("Friday");
+    break;
+  case 3:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid");
 }
