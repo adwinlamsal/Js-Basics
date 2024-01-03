d8beb2c60078640f8fcc0f2c55c4267eac719f5f
bhankumari <bhankumarilamsal137@gmail.com>
2024-01-03 08:43:38 +0545

    operators

--- variable/index.js
+++ variable/index.js
@@ -3,4 +3,12 @@ console.log("hello woerld")
 
 let personName="Krishna";
 
+
 console.log(personName);
+
+
+let krishna = Number(prompt("What is your birth year"))
+
+const value = 2024-krishna
+
+console.log(value)
